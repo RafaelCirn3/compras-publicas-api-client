aaf093c2941c025d14645080b4604376501ac8f1
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2026-04-02 21:06:24 -0300

    feat: adicionar configuração de arquivos estáticos e nova migração para opções de filtro

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
@@ -9,3 +11,6 @@ urlpatterns = [
     path("rpa/", include("apps.rpa.urls")),
     path("", RedirectView.as_view(pattern_name="filtros:dashboard", permanent=False)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
